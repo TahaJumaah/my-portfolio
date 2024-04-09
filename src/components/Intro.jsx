f128f4cd63ee54b8f0be9b410837e3dc6d62a686
Taha Jumaah <taharasheed95@gmail.com>
2024-04-09 21:52:47 +0200

    Trying intervals, gonna sleep now.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,24 @@
+import { useEffect } from "react";
 import styles from "./Intro.module.css";
-
+import { useState } from "react";
 export default function Intro() {
+  const [jobTitle, setJobTitle] = useState(null);
+
+  function changeTitle() {
+    function interval() {
+      setInterval(() => {
+        setJobTitle("sameersInterval");
+        console.log("sameersInterval");
+        interval();
+      }, 3000);
+    }
+    clearInterval(interval);
+  }
+  useEffect(changeTitle, []);
   return (
     <div className={styles.intro}>
       <h1>Taha Jumaah</h1>
-      <h2>Full Stack Web Developer</h2>
+      <h2>Full Stack {jobTitle} Developer</h2>
 
       <h2>
         <img src="src\assets\home_icon.svg"></img> Prague
