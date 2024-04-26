14d20946cf570f5a9420cdff52d81bd260d84aec
Taha Jumaah <taharasheed95@gmail.com>
2024-04-26 15:14:36 +0200

    Style and font updates

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,26 +1,12 @@
-import { useEffect } from "react";
 import styles from "./aboutMe.module.css";
 import { useState } from "react";
-import { HomeIcon } from "../assets/SVGs";
 export default function Intro() {
   const [jobTitle, setJobTitle] = useState(null);
 
-  function changeTitle() {
-    function interval() {
-      setInterval(() => {
-        setJobTitle("sameersInterval");
-        interval();
-      }, 3000);
-    }
-    clearInterval(interval);
-  }
-  useEffect(changeTitle, []);
   return (
     <div className={styles.intro}>
       <h1>Taha Jumaah</h1>
       <h2>Full Stack {jobTitle} Developer</h2>
-
-      <h2>{HomeIcon} Prague</h2>
     </div>
   );
 }
